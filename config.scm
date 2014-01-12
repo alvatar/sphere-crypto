75a59263625495a097072863ed6d76b609791d0a
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-12 19:59:38 +0100

    base-macros dependencies

--- config.scm
+++ config.scm
@@ -1,14 +1,17 @@
 (sphere: "crypto")
 (dependencies:
+ (aes
+  (include (core: base-macros)))
  (digest
-  (load (strings: u8)))
- (digest
+  (include (core: base-macros))
   (load (strings: u8)))
  (hmac
+  (include (core: base-macros))
   (load (fabric: algorithm/u8vector)
         (strings: u8)
         (= digest)))
  (rsa
+  (include (core: base-macros))
   (load (math: arithmetics)
         (fabric: algorithm/u8vector)
         (codec: util)
